d84bab7acc32720243cd3c8986a1f6f6f708af70
Jamie W <jamie.won.2000@gmail.com>
2019-02-03 15:49:11 -0500

    given data, will add the deal to the webpage. 2 ways to do it. Will change depending on future.

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ function getSBValue (event) {
 
 
 // adds event listeners to proper page elements when page loads
+// Since we're not hardcoding
 window.onload = function addListeners () {
 	// For detail panels
 	let buttons = document.querySelectorAll("button.detailButton");
@@ -22,6 +23,8 @@ window.onload = function addListeners () {
 	// for search button
 	let searchButton = document.querySelector("#searchButton");
 	searchButton.addEventListener('click', getSBValue);
+
+	addDealString("iTea", "BOGO", null, "I hope this works");
 }
 
 
@@ -31,17 +34,87 @@ window.onclick = function(event) {
 	if (!event.target.matches('.detailButton')) {
 
 	    let dropdowns = document.getElementsByClassName("detailPanel");
+	    let merchants = document.getElementsByClassName("merchant");
 	  	for (let i = 0; i < dropdowns.length; i++) {
 	   		if (!dropdowns[i].classList.contains('inactive')) {
 		    	dropdowns[i].classList.add('inactive');
 		    }
-		}
-		let merchants = document.getElementsByClassName("merchant");
-		for (let i = 0; i < merchants.length; i++) {
-	   		if (merchants[i].classList.contains('inactive')) {
+		    if (merchants[i].classList.contains('inactive')) {
 		    	merchants[i].classList.remove('inactive');
 		    }
 		}
+			
 	}
 }
+
+// If details is just a string
+function addDealString (merchant, deal, logoSrc, details) {
+	
+	// Div that contains everything
+	let div = document.createElement("div");
+	div.classList.add("item");
+
+	// Image chosen
+	let img = document.createElement("img");
+	img.classList.add("itemImage");
+	img.src = logoSrc;
+	
+	// Inner Div - details
+	let innerDiv = document.createElement("div");
+	innerDiv.classList.add("itemText");
+	innerDiv.innerHTML = "<strong>" + deal + "</strong>";
+	innerDiv.innerHTML += "<button class = \"detailButton\">Details</button>";
+	innerDiv.innerHTML += "<div class=\"detailPanel inactive\">" + details + "</div>";
+	innerDiv.innerHTML += "<p class =\"merchant\">" + merchant +"</p>";
+	
+	console.log(innerDiv.innerHTML);
+
+	// Adds event listener
+	let els = innerDiv.childNodes;
+	let button = els[1];
+	button.addEventListener('click', function () {
+		els[2].classList.remove("inactive");
+		els[3].classList.add("inactive");
+	});
+	
+	div.appendChild(img);
+	div.appendChild(innerDiv);
+	document.querySelector(".content .para").appendChild(div);
+}
+
+// If details is a map of detail type, then info ie location: rnandom address
+function addDealMap(details) {
+
+	// Div that contains everything
+	let div = document.createElement("div");
+	div.classList.add("item");
+
+	// Image chosen
+	let img = document.createElement("img");
+	img.classList.add("itemImage");
+	img.src = details.logoSrc;
+	
+	// Inner Div - details
+	let innerDiv = document.createElement("div");
+	innerDiv.classList.add("itemText");
+	innerDiv.innerHTML = "<strong>" + details.deal + "</strong>";
+	innerDiv.innerHTML += "<button class = \"detailButton\">Details</button>";
+	innerDiv.innerHTML += "<div class=\"detailPanel inactive\">" + details.details + "</div>";
+	innerDiv.innerHTML += "<p class =\"merchant\">" + details.merchant +"</p>";
+	
+	console.log(innerDiv.innerHTML);
+
+	// Adds event listener
+	let els = innerDiv.childNodes;
+	let button = els[1];
+	button.addEventListener('click', function () {
+		els[2].classList.remove("inactive");
+		els[3].classList.add("inactive");
+	});
+	
+	div.appendChild(img);
+	div.appendChild(innerDiv);
+	document.querySelector(".content .para").appendChild(div);
+}
+
 
