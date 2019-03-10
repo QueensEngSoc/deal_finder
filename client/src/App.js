b8a53a077a50719f04e2915a17ad408759802eef
Jamie W <jamie.won.2000@gmail.com>
2019-03-10 16:27:00 -0400

    things for React, and more styling - tis a neverending pain :3

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar/Navbar.js';
+import Gui from './Components/Gui/Gui.js';
+import Footer from './Components/Footer/Footer.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "entire">
+        <Navbar/>
+        <Gui/>
+        <Footer/>
       </div>
     );
   }
