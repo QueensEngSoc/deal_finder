9614fcf21f34872650e8e66b2e7f9c3d99ed993c
Jamie W <jamie.won.2000@gmail.com>
2019-03-31 11:46:38 -0400

    Forgot the footer, and also hypothetical thing to work for Deal

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer(props) {
+	return (
+		<footer className="py-5 bg-dark">
+			<div className="container">
+				<p className="m-0 text-center text-white">
+					Copyright &copy; Queen's University <br />Engineering
+					Society Software Development <br />Team 1
+				</p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
