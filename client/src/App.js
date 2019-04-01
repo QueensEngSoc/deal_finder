6e9efa20b24cddc0af0cea85308c99f5e3d82929
Jamie W <jamie.won.2000@gmail.com>
2019-04-01 15:50:32 -0400

    A test case, of pulled properly from a database

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,33 @@ import Gui from "./Components/Gui";  // Not used for now
 import Footer from "./Components/Footer";
 
 class App extends Component {
-  // state = { deals: [] }; // empty array, will pull from database later
-  state = { todos: [] };
+  constructor (props) {
+    super(props);
+    // this.state = { deals: [] }; // empty array, will pull from database later
+    this.state = { todos: [] };
+  //   this.state = {
+  //   deals: [
+  //     {
+  //       merchant: "merchant1",
+  //       dealName: "dealname1",
+  //       img: "imagePath1",
+  //       details: "details1"
+  //     },
+  //     {
+  //       merchant: "merchant2",
+  //       dealName: "dealname2",
+  //       img: "imagePath2",
+  //       details: "details2"
+  //     },
+  //     {
+  //       merchant: "merchant3",
+  //       dealName: "dealname3",
+  //       img: "imagePath3",
+  //       details: "details3"
+  //     }]
+  //   };
+  }
+  
   // Once component loads, pulls from database, will save in the state under deals
   // Information will later be used to replace a code segment
   // Add error path?
@@ -47,36 +72,36 @@ class App extends Component {
 //     );
 //   }
 
-  // render() {
-  //   return (
-  //     <div className="entire">
-  //       <Navbar />
-  //       {this.state.deals.map(deal => (
-  //         <div className="row">
-  //           <div className="col-md-7">
-  //             <a href="#">
-  //               <img
-  //                 className="img-fluid rounded mb-3 mb-md-0"
-  //                 src={deal.image}
-  //                 alt=""
-  //               />
-  //             </a>
-  //           </div>
-  //           <div className="col-md-5">
-  //             <div className="dNameButton">
-  //               <h3>{deal.dealName}</h3>
-  //               <a className="detailButton btn btn-primary" href="#">
-  //                 View Details
-  //               </a>
-  //             </div>
-  //             <p>{deal.details}</p>
-  //           </div>
-  //         </div>
-  //       ))}
-  //       <Footer />
-  //     </div>
-  //   );
-  // }
+//   render() {
+//     return (
+//       <div className="entire">
+//         <Navbar />
+//         {this.state.deals.map(deal => (
+//           <div className="row">
+//             <div className="col-md-7">
+//               <a href="#">
+//                 <img
+//                   className="img-fluid rounded mb-3 mb-md-0"
+//                   src={deal.image}
+//                   alt=""
+//                 />
+//               </a>
+//             </div>
+//             <div className="col-md-5">
+//               <div className="dNameButton">
+//                 <h3>{deal.merchant}</h3>
+//                 <a className="detailButton btn btn-primary" href="#">
+//                   View Details
+//                 </a>
+//               </div>
+//               <p>{deal.details}</p>
+//             </div>
+//           </div>
+//         ))}
+//         <Footer />
+//       </div>
+//     );
+//   }
 }
 
 export default App;
